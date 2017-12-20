67da15c7e36f8e84a17c1782243ba1310d921eb1
yonechen <yorkchan94@gmail.com>
2017-12-20 08:40:40 +0800

    mulitple-video-chat

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,7 @@
 class Page {
     constructor() {
         this.UI = {
+            group: document.querySelector('.group'),
             btn_call: document.getElementById('btn-call'),
             video_local: document.getElementById('video-local'),
             // video_remote: document.getElementById('video-remote')
@@ -17,21 +18,36 @@ class Page {
     bindEvent() {
         const { btn_call,video_local } = this.UI;
         btn_call.addEventListener('click', e => {
-            btn_call.disable = true;
-            const user = new User('wss://www.yonechen.com/wss/',Date.now() +'',this.stream,source => {
-                const video_remote = document.createElement('video');
-                video_remote.autoplay = true;
-                video_remote.width = 600;
-                video_remote.height = 600;
-                video_remote.src = source;
-                document.body.insertBefore(video_remote,video_local);
-            });
+            btn_call.classList.add('hide');
+            const url = 'wss://www.yonechen.com/wss/';// ws://127.0.0.1:8084
+            const user = new User(url,Date.now() +'',this.stream,this.createVideo.bind(this),this.deleteVideo.bind(this));
             //-----
             // const ws = new WebSocket('ws://127.0.0.1:8007');
             // ws.addEventListener('open', e => {
             // });
             //-----
         });
+    }
+    createVideo(token,source) {
+        const {group} = this.UI;
+        const box = document.createElement('div');
+        box.classList.add('box');
+        const title = document.createElement('span');
+        title.innerText = token;
+        title.classList.add('title');
+        const video = document.createElement('video');
+        video.classList.add('video');
+        video.autoplay = true;
+        video.src = source;
+        box.id = token;
+        box.appendChild(title);
+        box.appendChild(video);
+        group.appendChild(box);
+    }
+    deleteVideo(token) {
+        const {group} = this.UI;
+        const box = document.getElementById(token);
+        group.removeChild(box);
     }
     getMediaStream() {
         return navigator.mediaDevices.getUserMedia({
@@ -41,87 +57,114 @@ class Page {
     }
 }
 class User {
-    constructor(url,userID,stream,videoJoinCallback) {
-        this.userID = userID;
+    constructor(url,userID,stream,userJoinCallback,userLeaveCallback) {
         this.stream = stream;
+        this.userJoinCallback = userJoinCallback;
+        this.userLeaveCallback = userLeaveCallback;
         this.ws = new WebSocket(url);
-        this.state = {
-            localDescription: null,
-            remoteDescription: null,
+        this.peerSets = {};
+        this.ws.addEventListener('message',e => {
+            const msg = JSON.parse(e.data);
+            console.log(msg);
+            const {msg_type,content,tokenList,token} = msg;
+            switch(msg_type) {
+                case 'I_JOIN': this.createPeersAndOffers(tokenList);break; // 当我加入时，创建对其他人的连接，并发起offer
+                case 'OTHER_JOIN': this.addPeer(token);break; // 有新人加入时，我收到通知，建立一条新连接
+                case 'CANDIDATE': this.addCandidate(token,content.icecandidate);break; // 当收到 token用户 发来的candidate地址，设置
+                case 'OFFER': this.answer(token,content.offer);break; // 当收到 token用户 发来的offer时，回送answer
+                case 'ANSWER': this.receiveAnswer(token,content.answer);break;
+            }
+        });
+        // this.peer.oniceconnectionstatechange = function(e) {
+        // // onIceStateChange(pc1, e);
+        // };
+    }
+    createPeersAndOffers(tokenList) {
+        const othersNumber = tokenList.length;
+        if (othersNumber < 1 || othersNumber > 10) return; //两个人在线上时才需要建立连接
+        for (let i = 0; i < othersNumber; i++) { //n个人在线时，建立 n-1个连接
+            const token = tokenList[i];
+            this.addPeer(token); // 创建对 token用户 的连接
+            this.offer(token); // 发送对 token用户 的offer
         }
-        var config = {
+    }
+    addPeer(token) {
+        const {stream,userJoinCallback,userLeaveCallback} = this;
+        const config = {
             'iceServers': [{ 'url': 'stun:stun.services.mozilla.com' }, { 'url': 'stun:stunserver.org' }, { 'url': 'stun:stun.l.google.com:19302' }]
         };
-        this.peer = new RTCPeerConnection(config);
-        this.peer.addEventListener('icecandidate',e => {
-            debugger
-            const info = {
-                userID,
-                icecandidate: e.candidate
+        const peer = new RTCPeerConnection(config);
+        peer.addEventListener('icecandidate',e => { //在stun查询到自己的ip外网地址时，发送给 token用户
+            const msg = {
+                msg_type: 'CANDIDATE',
+                token,
+                content: {
+                    icecandidate: e.candidate
+                }
             };
-            this.ws.send(JSON.stringify(info));
+            this.wsSend(msg);
           });
-        this.ws.addEventListener('message',e => {
-            debugger
-            const data = JSON.parse(e.data);
-            console.log(data);
-            const {icecandidate,offer,answer,onlineNumber} = data;
-            if (onlineNumber && onlineNumber >1) this.offer();
-            if(icecandidate) this.peer.addIceCandidate(new RTCIceCandidate(icecandidate));
-            if(offer) this.answer(offer);
-            if(answer) this.receiveAnswer(answer);
+        peer.addStream(stream);
+        peer.addEventListener('addstream', e => {
+            userJoinCallback(token,window.URL.createObjectURL(e.stream));
         });
-        this.peer.addStream(stream);
-        this.peer.addEventListener('addstream', e => {
-            videoJoinCallback(window.URL.createObjectURL(e.stream));
-
-        });
-        this.peer.oniceconnectionstatechange = function(e) {
-        // onIceStateChange(pc1, e);
-        };
+        peer.addEventListener('iceconnectionstatechange', e => {
+            if(peer.iceConnectionState == 'disconnected') {
+                console.log(token + 'Disconnected');
+                userLeaveCallback(token);
+            }
+        })
+        this.peerSets[token] = peer;
     }
-    async offer() { // 我是请求方 发起offer并设置本身的SDP信息
-        const {peer,ws,state,userID} = this;
-        const offer = await this.peer.createOffer({
+    addCandidate(token,icecandidate) {
+        const peer = this.peerSets[token];
+        peer.addIceCandidate(new RTCIceCandidate(icecandidate));
+    }
+    async offer(token) { // 我是请求方 发起offer并设置本身的SDP信息
+        // const {peerSets,ws,state,userID} = this;
+        const peer = this.peerSets[token];
+        const offer = await peer.createOffer({
             offerToReceiveAudio: 1,
             offerToReceiveVideo: 1
         });
         await peer.setLocalDescription(offer);
-        this.state.localDescription = offer;
-        debugger
-        const info = {
-            userID,
-            offer
+        const msg = {
+            msg_type: 'OFFER',
+            token,
+            content: {
+                offer
+            }
         };
-        ws.send(JSON.stringify(info));
-            // //对方接收到这个offer
-            // theirConnection.setRemoteDescription(offer);
-            // //对方产生一个answer
-            // theirConnection.createAnswer().then(answer => {
-            //     theirConnection.setLocalDescription(answer);
-            //     //本方接收到一个answer
-            //     yourConnection.setRemoteDescription(answer);
-            // })
+        this.wsSend(msg);
     }
-    async answer(offer) { // 我是响应方 收到请求方offer后，设置对方的SDP信息，发起answer，设置本身的SDP信息
-        const {peer,ws,state,userID} = this;
+    async answer(token,offer) { // 我是响应方 收到请求方offer后，设置对方的SDP信息，发起answer，设置本身的SDP信息
+        // const {peer,ws,state,userID} = this;
+        const peer = this.peerSets[token];
         await peer.setRemoteDescription(offer);
-        this.state.remoteDescription = offer;
+        // this.state.remoteDescription = offer;
 
         const answer = await peer.createAnswer();
         await peer.setLocalDescription(answer);
-        this.state.localDescription = answer;
+        // this.state.localDescription = answer;
 
-        const info = {
-            userID,
-            answer
+        const msg = {
+            msg_type: 'ANSWER',
+            token,
+            content: {
+                answer
+            }
+            
         };
-        ws.send(JSON.stringify(info));
+        this.wsSend(msg);
     }
-    async receiveAnswer(answer) { // 我是请求方 收到响应方answer后，设置对方的SDP信息
-        const {peer,ws,state} = this;
+    async receiveAnswer(token,answer) { // 我是请求方 收到响应方answer后，设置对方的SDP信息
+        // const {peer,ws,state} = this;
+        const peer = this.peerSets[token];
         await peer.setRemoteDescription(answer);
-        this.state.remoteDescription = answer;
+        // this.state.remoteDescription = answer;
+    }
+    wsSend(msg) {
+        this.ws.send(JSON.stringify(msg));
     }
 }
 // javascript code
